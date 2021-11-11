a3b783fa2b08d9e2e6d541a3ca944b4d2d675795
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:29:03 +0200

    datafromfile

---
+++ DataFromFile/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite color:");
+            string userColor = Console.ReadLine();
+
+            string output = GetLinesFromfileData(userColor);
+
+            if(output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Sorry. Color not found.");
+            }
+
+            
+            
+
+        }
+
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\Users\opilane\Samples";
+            string fileName = "personality.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+
+            return dataFromFile;
+        }
+
+        private static string GetLinesFromfileData(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+
+            foreach (string line in dataFromFile)
+            {
+                if( line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+
+    
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your zodiac:");
+            string userZodiac = Console.ReadLine();
+
+            string output = (userZodiac);
+
+            if (output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Sorry. Color not found.");
+            }
+
+        }
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\Users\opilane\Samples";
+            string fileName = "horoscope.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+
+            return dataFromFile;
+        }
+
+        private static string GetLinesFromfileData(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+
+            foreach (string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+            }
+            return result;
+        }
+
+    }
